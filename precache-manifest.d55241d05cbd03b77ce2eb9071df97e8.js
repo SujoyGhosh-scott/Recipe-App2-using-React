a0995e6e0f085dff3a49f3a476f7407fcd06bb84
SujoyGhosh-scott <ghoshsujoy336@gmail.com>
2020-05-15 22:29:53 +0530

    Updates

---
+++ precache-manifest.d55241d05cbd03b77ce2eb9071df97e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a0bc444c85a61f671f743fc8aa5366",
+    "url": "/Recipe-App-using-React/index.html"
+  },
+  {
+    "revision": "a9cccf5533923a3fca90",
+    "url": "/Recipe-App-using-React/static/css/main.86efbc47.chunk.css"
+  },
+  {
+    "revision": "a417c8ad5fe2647c0cba",
+    "url": "/Recipe-App-using-React/static/js/2.dddb0523.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-App-using-React/static/js/2.dddb0523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9cccf5533923a3fca90",
+    "url": "/Recipe-App-using-React/static/js/main.12a4b71f.chunk.js"
+  },
+  {
+    "revision": "ea73485f60daf6cad10e",
+    "url": "/Recipe-App-using-React/static/js/runtime-main.db8572c6.js"
+  }
+]);
