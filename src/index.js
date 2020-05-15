c28fae2ff53f8cec08d94cb8e244608fbcaf6c17
SujoyGhosh-scott <ghoshsujoy336@gmail.com>
2020-05-15 22:29:24 +0530

    adding all files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,114 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Recipe extends React.Component {
+  state = { show: false };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  handelClick = () => {
+    this.setState({
+      show: !this.state.show,
+    });
+  };
+
+  render() {
+    return (
+      <div className="recipe">
+        <div className="all-text">
+          <h2>{this.props.title}</h2>
+          <p>
+            <b>calories: </b>
+            {this.props.calories.toFixed(2)}
+          </p>
+          <button className="ing-button" onClick={this.handelClick}>
+            <b>ingredients</b>
+          </button>
+          {this.state.show && (
+            <ol>
+              {this.props.ingredients.map((ingredient) => (
+                <li>{ingredient.text}</li>
+              ))}
+            </ol>
+          )}
+        </div>
+        <img src={this.props.image} alt="" />
+      </div>
+    );
+  }
+}
+
+function App() {
+  const App_Id = "b20d8375";
+  const App_Key = "11d9e602372119f76ec7d91076087c1a";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+
+  let flag = 1;
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${App_Id}&app_key=${App_Key}`
+    );
+    const data = await response.json();
+    //console.log(data.hits);
+    setRecipes(data.hits);
+    //console.log(recipes);
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+    //console.log(search);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
+  return (
+    <div className="App">
+      <div className="navbar">
+        <h1>Recipe App</h1>
+        <div className="nav-form">
+          <form className="search-form" onSubmit={getSearch}>
+            <input
+              className="search-bar"
+              type="text"
+              value={search}
+              onChange={updateSearch}
+              placeholder="Search Food"
+            />
+            <button
+              type="submit"
+              onClick={getRecipes}
+              className="search-button"
+              type="submit"
+            >
+              <i class="fas fa-search"></i>
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
